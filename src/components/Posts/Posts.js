79354dbe8a49de712479c548c336e9ab5b0dc3b1
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-08-24 14:23:02 -0400

    Working Through Passing The Props

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,6 +3,7 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
+  console.log(props)
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
@@ -10,6 +11,7 @@ const Posts = (props) => {
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
       {/* Check the implementation of Post to see what props it requires! */}
+     {Post.map}
     </div>
   );
 };
